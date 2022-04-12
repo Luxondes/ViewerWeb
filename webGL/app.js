f94578b1cddb329246b3613f7d6385e0562086d3
Julien Gelles <julien.gelles.etu@univ-lille.fr>
2022-04-12 13:00:15 +0200

    triangle + color

--- webGL/app.js
+++ webGL/app.js
@@ -1,15 +1,21 @@
 var vertexShaderText = 
 [
+'precision mediump float;',
 'attribute vec3 position;',
+'attribute vec3 color;',
+'varying vec3 vColor;',
 'void main(){',
+'  vColor = color;',
 '  gl_Position = vec4(position, 1);',
 '}'
 ].join('\n');
 
 var fragmentShaderText =
 [
+'precision mediump float;',
+'varying vec3 vColor;',
 'void main(){',
-'  gl_FragColor = vec4(1, 0.7, 0.4, 1);',
+'  gl_FragColor = vec4(vColor, 1);',
 '}'
 ].join('\n');
 
@@ -38,10 +44,20 @@ const main = function() {
          1,  1,  0   //v3
     ];
 
-    //Buffer
-    const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    const colorData = [
+        1,  0,  0,  //v1.color
+        0,  1,  0,  //v2.color
+        0,  0,  1   //v3.color
+   ];
+
+    //Buffers
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
     //Shaders
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -58,9 +74,16 @@ const main = function() {
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
 
+    //
     const positionLocation = gl.getAttribLocation(program, `position`);
     gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+    const colorLocation = gl.getAttribLocation(program, `color`);
+    gl.enableVertexAttribArray(colorLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
     gl.useProgram(program);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
